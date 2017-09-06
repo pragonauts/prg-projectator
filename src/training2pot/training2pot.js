d2d378bf6b1022e92b809af1b97f6a2d6a085973
Václav Oborník <register.ufon@gmail.com>
2017-09-06 14:59:35 +0200

    training2pot duplication alerts

--- src/training2pot/training2pot.js
+++ src/training2pot/training2pot.js
@@ -6,13 +6,25 @@ function training2pot (jsonPath, targetPath) {
 
     const json = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
 
+    const map = {};
+
     const pot = json.rasa_nlu_data.common_examples
+        .reduce((prev, item) => {
+            if (!map[item.text]) {
+                map[item.text] = true;
+                prev.push(item);
+            } else {
+                console.error('DUPLICATION DETECTED:', item.text);
+            }
+            return prev;
+        }, [])
         .map(item => `
             #. ${JSON.stringify({ intent: item.intent, entities: item.entities })}
             msgid "${item.text}"
             msgstr "${item.text}"
         `.replace(/\n\s+/g, '\n'))
-        .join('\n');
+        .join('\n')
+        .replace(/\n\n\n/g, '\n\n');
 
     fs.writeFileSync(targetPath, pot, 'utf8');
 }
