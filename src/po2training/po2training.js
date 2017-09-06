f669d1587258d79c760741537bb0a2568fbd8b64
Václav Oborník <register.ufon@gmail.com>
2017-09-06 15:26:19 +0200

    fix of po2training for cases with missing translations

--- src/po2training/po2training.js
+++ src/po2training/po2training.js
@@ -6,14 +6,15 @@ function training2pot (poPath, targetPath) {
 
     const poContent = fs.readFileSync(poPath, 'utf8');
 
-    const translationMatcher = /#\.\s*(\{.+?\})(?:\n|.)+?msgstr "(.+?)"\n/im;
+    const translationMatcher = /#\.\s*(\{.+?\})(?:\n|.)+?msgstr "(.*?)"\n/im;
 
     const items = poContent.match(new RegExp(translationMatcher, 'g'))
         .map((poItem) => {
             const [, definition, text] = poItem.match(translationMatcher);
             const { intent, entities } = JSON.parse(definition);
             return { text, intent, entities };
-        });
+        })
+        .filter(item => !!item.text);
 
     const trainingContent = {
         rasa_nlu_data: {
